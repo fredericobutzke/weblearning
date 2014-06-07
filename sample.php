4c1d4154d58d48b7e264ccaa60adb3dc5c764cae
fredericobutzke <butzkefrederico@gmail.com>
2014-06-07 01:44:32 -0700

    Delete sample.php

--- sample.php
+++
@@ -1,6 +0,0 @@
-<?php $to = "frederico.butzke@aiesec.net";
-      $subject = "Registration";
-      $body = "BODY";
-      $headers = "From: butzkefrederico@gmail.com\r\n";
-      mail($to, $subject, $body, $headers);
-?>
